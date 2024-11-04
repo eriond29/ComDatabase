29264aa58b77d997ef7194b828d11b8f41915736
stephen.torrez@umontana.edu <stephen.torrez@umontana.edu>
2024-11-04 04:16:06 +0000

    Project: HTML From a Database Table

--- HTML_From_Server/HTMLCharacterApp.py
+++ HTML_From_Server/HTMLCharacterApp.py
@@ -9,31 +9,31 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
+
 def showSpeakers():
     with open('/home/googlabill/ComDatabase/HTML_From_Server/secrets.json', 'r') as secretFile:
         creds = json.load(secretFile)['mysqlCredentials']
 
     connection = mysql.connector.connect(**creds)
-
+    
     mycursor = connection.cursor()
 
     # If there is a name and desc 'GET' variable, insert the new value into the database
+    newCharacterID = request.args.get('characterid')
     newFirst = request.args.get('firstname')
     newLast = request.args.get('lastname')
-    newLast = request.args.get('lastname')
-    newLast = request.args.get('lastname')
-    if newFirst is not None and newLast is not None:
-        mycursor.execute("INSERT into ComicCharacter (CharacterID, FirstName, LastName, Monicker) values (%s, %s)", (CharacterID, FirstName, LastName, Monicker))
+    newMonicker = request.args.get('monicker')  # Optional field for monicker
+    if newCharacterID and newFirst and newLast and newMonicker is not None:
+        insert_query = "INSERT INTO ComicCharacter (CharacterID, FirstName, LastName, Monicker) VALUES (%s, %s, %s, %s)"
+        mycursor.execute(insert_query, (newCharacterID, newFirst, newLast, newMonicker))
         connection.commit()
 
     # Fetch the current values of the speaker table
-    mycursor.execute("Select CharacterID, FirstName, LastName, Monicker FROM ComicCharacter")
+    mycursor.execute("SELECT CharacterID, FirstName, LastName, Monicker FROM ComicCharacter")
     myresult = mycursor.fetchall()
     mycursor.close()
     connection.close()
-    return render_template('actor-list.html', collection=myresult)
-    #return "Hello world"
-
+    return render_template('Comic Characters.html', collection=myresult)
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True, host="0.0.0.0")
